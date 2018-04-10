9ba1e7c4dd689fce9238feeb3f0d95eebd4ad14c
Anna Piwnik <a.piwnik@o2.pl>
2018-04-10 21:27:04 +0200

    Start task

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+
+const plugins: 
+    [new HtmlWebpackPlugin({
+        template: 'client/index.html',
+        filename: 'index.html',
+        inject: 'body',
+        })
+    ],
+    new webpack.optimize.UglifyJsPlugin(),
+    new OptimizeJsPlugin({
+        sourceMap: false
+    })
+
+module.exports = {
+    entry: './client/index.js',
+        output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+        {
+            test: /\.js$/,
+            loader: "babel-loader"
+        },
+        {
+            test: /\.css$/,
+            use: [
+                { loader: 'style-loader'},
+                {
+                    loader: 'css-loader',
+                    options: {
+                        modules: true
+                    }
+                }
+            ]
+        }
+        ]
+    },
+    devServer: {
+        proxy: {
+            '/socket.io': {
+                target: 'http://localhost:3000',
+                ws: true
+            }
+        }
+    }
+};
