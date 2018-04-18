f1ac24de363ecd9b6ba078d9e5d2d27887094089
Anna Piwnik <a.piwnik@o2.pl>
2018-04-18 14:44:50 +0200

    Commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path');
 
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-var OptimizeJsPlugin = require('optimize-js-plugin');
+// var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+// var OptimizeJsPlugin = require('optimize-js-plugin');
 
-const plugins: 
+const plugins = 
     [new HtmlWebpackPlugin({
         template: 'client/index.html',
         filename: 'index.html',
         inject: 'body',
         })
-    ],
-    new webpack.optimize.UglifyJsPlugin(),
-    new OptimizeJsPlugin({
-        sourceMap: false
-    })
+    // new webpack.optimize.UglifyJsPlugin(),
+    // new OptimizeJsPlugin({
+    //     sourceMap: false
+    // })
+    ];
 
 module.exports = {
     entry: './client/index.js',
